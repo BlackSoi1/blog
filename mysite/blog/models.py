50a1ba0e25421d3c7d9d26a03f9518001dcaff34
Xiaolong Li <lixiaolongjack@gmail.com>
2021-07-05 15:49:21 +0800

    Fix bugs the website works

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+# SuperUserInformation
+# User: xiaolongli
+# Email:
+# Password: li123911..
+
 # Create your models here.
-
-
 class Post(models.Model):
-    # author connect the superuser of the website
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -17,11 +19,8 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(aprroved_comment=True)
+        return self.comments.filter(approved_comment=True)
 
-    # Once create the instance of Post what should I do
-    # pk primary key
-    # go to post_detail page
     def get_absolute_url(self):
         return reverse("post_detail",kwargs={'pk':self.pk})
 
@@ -30,21 +29,18 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.post', related_name='comments')
-    # this author can be anyone
+    post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
-    # Should match with approve_comments()
-    approve_comment = models.BooleanFiled(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approve_comment=True
+        self.approved_comment = True
         self.save()
 
-    # go back to main home page to all post lists
     def get_absolute_url(self):
-        return reverse('post_list')
+        return reverse("post_list")
 
     def __str__(self):
         return self.text
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,97 +1,99 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from blog.models import Post, Comment
-from blog.forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
+
 from django.urls import reverse_lazy
-from django.views.generic import (TemplateView, ListView, DetailView, Createview,
-                                  UpdateView, DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-
-
 class AboutView(TemplateView):
-    template_name = 'about.html'
+    template_name = 'blog/about.html'
 
 
 class PostListView(ListView):
     model = Post
 
-    # lte = less than or equal to
-    # '-published_date' 从小到大排序
     def get_queryset(self):
-        return Post.objects.filter(publish_date__lte=timezone.now()).order_by('-published_date')
-
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
 
 
-class CreatePostView(LoginRequiredMixin, Createview):
-    login_url = '/login'
-    redirct_field_name = 'blog/post_detail.html'
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
 
     form_class = PostForm
+
     model = Post
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
-    login_url = '/login'
-    redirct_field_name = 'blog/post_detail.html'
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
 
     form_class = PostForm
+
     model = Post
 
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin,DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
 
 
-class DraftListView(LoginRequiredMixin, ListView):
-    login_url = '/login'
-    redirct_field_name = 'blog/post_list.html'
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_draft_list.html'
+
     model = Post
 
-    # Draft doesnt have published date
     def get_queryset(self):
-        return Post.objects.filter(publish_date__isnull=True).order_by('create_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
-####################################
-####################################
-####################################
+
+#######################################
+## Functions that require a pk match ##
+#######################################
 
 @login_required
-def post_publish(request,pk):
-    post=get_object_or_404(Post,pk=pk)
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail',pk=pk)
+    return redirect('post_detail', pk=pk)
 
-# Function views about comments
 @login_required
 def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post,pk=pk)
-    if request.method =='POST':
-        form =CommentForm(request.POST)
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post=post
+            comment.post = post
             comment.save()
-            return redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request,'blog/comment_form.html',{'form':form})
+    return render(request, 'blog/comment_form.html', {'form': form})
+
 
 @login_required
-def comment_approve(request,pk):
-    comment = get_object_or_404(Comment,pk=pk)
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
-    return redirect('post_detail',pk=comment.post.pk)
+    return redirect('post_detail', pk=comment.post.pk)
+
 
 @login_required
-def comment_remove(request,pk):
-    comment=get_object_or_404(Comment,pk=pk)
-    post_pk=comment.post.pk
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
     comment.delete()
-    return redirect('post_detail',pk=post_pk)
+    return redirect('post_detail', pk=post_pk)
